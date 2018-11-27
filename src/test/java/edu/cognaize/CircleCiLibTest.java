ff315d0b1abbb2d0e9bf3584d170e16529adca01
Martin <martin.topchyan@gmail.com>
2018-11-27 13:14:40 +0400

    added settings.xml

---
+++ src/test/java/edu/cognaize/CircleCiLibTest.java
@@ -0,0 +1,15 @@
+package edu.cognaize;
+
+
+import static junit.framework.TestCase.assertTrue;
+
+import org.junit.Test;
+
+public class CircleCiLibTest {
+
+  @Test
+  public void test(){
+    System.out.println("Everything is ok");
+    assertTrue(true);
+  }
+}
